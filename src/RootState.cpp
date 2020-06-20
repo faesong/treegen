62d54f5ef65ae72c395741b6c02507d1221eae44
Vitalii Minnakhmetov <restlessmonkey@ya.ru>
2020-06-20 12:39:52 +0300

    split RootState to .cpp and .hpp

---
+++ src/RootState.cpp
@@ -0,0 +1,70 @@
+// This is an independent project of an individual developer. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
+
+
+#include "RootState.hpp"
+
+
+#include "AppSettings.hpp"
+#include "SettingsUi.hpp"
+
+
+RootState::RootState (VcppBits::StateManager *pStateManager,
+                      AppSettings *pSettings)
+    : _stateMgr (pStateManager),
+      _settings (pSettings) {
+}
+
+void RootState::load () {
+}
+
+void RootState::resume () {
+    _stateMgr->popState();
+}
+
+void RootState::freeze (IState*) {
+}
+
+void RootState::unload () {
+}
+
+void RootState::update (const float /*pTimeStep*/) {
+    renderDockSpaceUi();
+    renderSettingsUi();
+}
+
+void RootState::renderDockSpaceUi () {
+    ImGuiViewport* viewport = ImGui::GetMainViewport();
+    ImGui::SetNextWindowPos(viewport->Pos);
+    ImGui::SetNextWindowSize(viewport->Size);
+
+    ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
+    ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.f);
+    ImGui::Begin("DockSpace",
+                 nullptr,
+                 ImGuiWindowFlags_NoBackground
+                 |ImGuiWindowFlags_NoBringToFrontOnFocus
+                 | ImGuiWindowFlags_NoInputs
+                 // TODO5: there has to be either title or menu bar
+                 // otherwise docking doesn't work as intended
+                 // (try to update IMGUI?)
+                 | ImGuiWindowFlags_MenuBar
+                 | ImGuiWindowFlags_NoTitleBar);
+
+
+    static ImGuiDockNodeFlags dockspace_flags =
+        ImGuiDockNodeFlags_PassthruCentralNode;
+    ImGuiID dockspace_id = ImGui::GetID("MyDockSpace");
+    ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), dockspace_flags);
+    ui::PopStyleVar(); // rounding
+    ui::PopStyleVar(); // padding
+
+    ImGui::End();
+}
+
+void RootState::renderSettingsUi () {
+    if (ui::Begin("Settings", 0, 0)) {
+        render_settings2_ui (&_settings->_settings, nullptr);
+    }
+    ui::End();
+}
--- src/RootState.hpp
+++ src/RootState.hpp
@@ -1,77 +1,31 @@
-#include <VcppBits/StateManager/IState.hpp>
+#pragma once
+
+
 #include <VcppBits/StateManager/StateManager.hpp>
 
-#include "SettingsUi.hpp"
-#include "AppSettings.hpp"
+
+#include <UrhoBits/UrhoAppFramework/IUpdatedState.hpp>
+
+
+class AppSettings;
 
 class RootState final : public UrhoBits::IUpdatedState {
 public:
     RootState (VcppBits::StateManager *pStateManager,
-        AppSettings *pSettings)
-        : _stateMgr(pStateManager),
-          _settings (pSettings) {
-
-    }
+               AppSettings *pSettings);
 
     bool alwaysUpdate () override { return true; }
 
-    void load () override {
-    }
-    void resume () override {
-        _stateMgr->popState();
-    }
-
-    void freeze (IState*) override {
-
-    }
-
-    void unload () override {
-
-    }
-
+    void load () override;
+    void resume () override;
+    void freeze (IState*) override;
+    void unload () override;
     std::string getName () const override { return "RootState"; }
 
-    void update (const float /*pTimeStep*/) override {
-        renderDockSpaceUi();
-        renderSettingsUi();
-    }
+    void update (const float /*pTimeStep*/) override;
 
-    void renderDockSpaceUi () {
-        ImGuiViewport* viewport = ImGui::GetMainViewport();
-        ImGui::SetNextWindowPos(viewport->Pos);
-        ImGui::SetNextWindowSize(viewport->Size);
-
-        ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
-        ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.f);
-        ImGui::Begin("DockSpace",
-                     nullptr,
-                     ImGuiWindowFlags_NoBackground
-                     |ImGuiWindowFlags_NoBringToFrontOnFocus
-                     | ImGuiWindowFlags_NoInputs
-                     // TODO5: there has to be either title or menu bar
-                     // otherwise docking doesn't work as intended
-                     // (try to update IMGUI?)
-                     | ImGuiWindowFlags_MenuBar
-                     | ImGuiWindowFlags_NoTitleBar);
-
-
-        static ImGuiDockNodeFlags dockspace_flags =
-             ImGuiDockNodeFlags_PassthruCentralNode;
-        ImGuiID dockspace_id = ImGui::GetID("MyDockSpace");
-        ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), dockspace_flags);
-        ui::PopStyleVar(); // rounding
-        ui::PopStyleVar(); // padding
-
-        ImGui::End();
-    }
-
-    void renderSettingsUi () {
-        if (ui::Begin("Settings", 0, 0)) {
-            render_settings2_ui (&_settings->_settings, nullptr);
-        }
-        ui::End();
-    }
-
+    void renderDockSpaceUi ();
+    void renderSettingsUi ();
 private:
     VcppBits::StateManager *_stateMgr;
     AppSettings *_settings;
